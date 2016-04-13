0e36f552b06ac49cdad992712abfe5e634c71c75
Xiang Zhang <zxteloiv@outlook.com>
2016-04-13 21:32:42 +0800

    adapt to the changed notebook name, add new note on how to read a paper

--- publish_notes.sh
+++ publish_notes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 quiver2jekyll.py ~/Quiver.qvlibrary/3E9A036B-C1C3-4B34-9934-3376137AC846.qvnotebook/ ~/code/blog/wiki
-quiver2jekyll.py ~/Quiver.qvlibrary/9538FF32-3F1B-4E9E-8B56-5A1D4DBE0EE2.qvnotebook/ ~/code/blog/wiki
+quiver2jekyll.py ~/Quiver.qvlibrary/9538FF32-3F1B-4E9E-8B56-5A1D4DBE0EE2.qvnotebook/ ~/code/blog/wiki --title nlp
 quiver2jekyll.py ~/Quiver.qvlibrary/B49E00B3-0E2E-4E02-BBD8-B040CF649491.qvnotebook/ ~/code/blog/wiki
 quiver2jekyll.py ~/Quiver.qvlibrary/E61EFA66-68FA-4118-A02F-F10951AFDC10.qvnotebook/ ~/code/blog/wiki
 quiver2jekyll.py ~/Quiver.qvlibrary/F3B3E4A8-ADE3-496F-BD02-5BE13BAB0F83.qvnotebook/ ~/code/blog/wiki
